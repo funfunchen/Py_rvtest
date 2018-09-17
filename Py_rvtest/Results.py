f1e5e9abee08306a518859c73ced46da1f9b02d5
funfunchen <funfun.chen@gmail.com>
2018-09-16 20:31:01 -0400

    add HDF5Store class reconstruct hdf5 resize add import modules

--- Py_rvtest/Results.py
+++ Py_rvtest/Results.py
@@ -10,7 +10,6 @@ class SaveData:
     def save_csv(self):
         self._res.to_csv(self._out, sep='\t', index=False, header=True)
 
-
 class HDF5Store:
     """
     Simple class to append > 2D value to a hdf5 file on disc
@@ -18,7 +17,7 @@ class HDF5Store:
     Params:
         d_path: filepath of h5 file
         d_set: dataset name within the file
-        d_shape: dataset shape (not counting main/batch axis)
+        d_shape: dataset coulumn dim(not counting main/batch axis)
         d_type: numpy dtype
 
     Usage:
@@ -34,30 +33,42 @@ class HDF5Store:
     def __init__(self, d_path, d_set, d_shape, d_type=np.float64):
         self._d_path = d_path
         self._d_set = d_set
-        self._d_shape = d_shape
+        self._d_col = d_shape  # d_shape should be a tuple
         self._row = 0
         self._d_type = d_type
 
         with h5py.File(self._d_path, mode='w') as h5f:
             self.dset = h5f.create_dataset(
                 self._d_set,
-                shape=(0, ) + self._d_shape[1:],
-                maxshape=(None, ) + self._d_shape[1:],
+                # shape=(0, ) + self._d_shape[1:],
+                shape=(0, ) + self._d_col,
+                maxshape=(None,) + self._d_col,
                 dtype=self._d_type)
 
     def append(self, arr):
         with h5py.File(self._d_path, mode='a') as h5f:
             dset = h5f[self._d_set]
-            dset.resize((self._row + arr.shape[0],) + arr.shape[1:])
+            dset.resize((self._row + arr.shape[0], ) + self._d_col)
             dset[self._row:, :] = arr
+            self._row += arr.shape[0]
+
+    def append_1d(self, arr):
+        with h5py.File(self._d_path, mode='a') as h5f:
+            dset = h5f[self._d_set]
+            dset.resize((self._row + arr.shape[0], ) + self._d_col)
+            dset[self._row:, ] = arr
             self._row += arr.shape[0]
 
 
 def main():
-    shape = (20, 20, 3)
-    hdf5_store = HDF5Store('/tmp/hdf5_store.h5', 'X', d_shape=shape)
+    shape_1 = (20,)
+    shape_2 = (20, 20, 3)
+    hdf5_store_1 = HDF5Store('/tmp/hdf5_store.h5', 'X', d_shape=())
     for _ in range(10):
-        hdf5_store.append(np.random.random(shape))
+        hdf5_store_1.append_1d(np.random.random(20))
+    hdf5_store_2 = HDF5Store('/tmp/hdf5_store.h5', 'Y', d_shape=(20, 3))
+    for _ in range(10):
+        hdf5_store_2.append(np.random.random(shape_2))
 
 
 if __name__ == '__main__':
--- Py_rvtest/__main__.py
+++ Py_rvtest/__main__.py
@@ -1,7 +1,9 @@
-from Py_rvtest.DataConsolidator import *
+import Py_rvtest.DataConsolidator as Dc
 import argparse
 from tqdm import tqdm
+import h5py
 import pandas as pd
+import numpy as np
 import Py_rvtest.Regression as Reg
 import Py_rvtest.Results as Res
 import Py_rvtest.MinimalSufficient as MinS
@@ -14,14 +16,16 @@ parser.add_argument("--chunk", type=int, help="the size of a chunk")
 parser.add_argument("--ped", "-p", help="phenotype file, ped format")
 args = parser.parse_args()
 
-ge = H5pyToMatrix(args.geno)
-pheno = PheToMatrix(args.ped)
+ge = Dc.H5pyToMatrix(args.geno)
+pheno = Dc.PheToMatrix(args.ped)
 y_samples = pheno.get_id
 
 if ge.check_sample_matched(y_samples):
     mat_index = None
+    print("Samples' number matched. Analysis is using all the samples.")
 else:
     mat_index = ge.match_index(y_samples)  # match the index, y_sample should be a list
+    print("Samples' number do not match, using the intersection ones.")  # len(y_samples) - len(mat_index)
 
 
 snps = ge.get_snp
@@ -45,28 +49,28 @@ with h5py.File("temp_min.h5", "w") as f:
     f.create_dataset("min_cc", data=pheno_min_cc, dtype=np.float64)
     pheno_min_tt = None
 
-    dset_gy = f.create_dataset("min_gy", d_shape=(0, ), maxshape=(None, ), d_type=np.float64)
-    dset_gg = f.create_dataset("min_gg", d_shape=(0, ), maxshape=(None, ), d_type=np.float64)
-    dset_gz = f.create_dataset("min_gz", d_shape=(0, cos_col), maxshape=(None, cos_col), d_type=np.float64)
-    for i in tqdm(range(0, snp_num, chunk)):
-        # Todo
-        # iterate the object to get matrix and compute
-        # snp_id = snps[i, i+chunk]
-        mat = ge.get_geno(i, i+chunk, mat_index)
-        geno_min = MinS.GenoMinSuffStat(mat, traits, cos)
-        dset_gy.resize((i+geno_min.get_GmY.shape[0], ))
-        dset_gy[i:, ] = geno_min.get_GmY
-        dset_gg.resize((i + geno_min.get_GmGm.shape[0], ))
-        dset_gg[i:, ] = geno_min.get_GmGm
-        dset_gz.resize((i + geno_min.get_GmZc.shape[0], cos_col))
-        dset_gz[i:, cos_col] = geno_min.get_GmZc
+dset_gy = Res.HDF5Store('/tmp/hdf5_min_gy.h5', "min_gy", d_shape=())
+dset_gg = Res.HDF5Store('/tmp/hdf5_min_gg.h5', "min_gg", d_shape=())
+dset_gz = Res.HDF5Store('/tmp/hdf5_min_gz.h5', "min_gz", d_shape=(cos_col, ))
+for i in tqdm(range(0, snp_num, chunk)):
+    # Todo
+    # iterate the object to get matrix and compute
+    # snp_id = snps[i, i+chunk]
+    mat = ge.get_geno(i, i+chunk, mat_index)
+    geno_min = MinS.GenoMinSuffStat(mat, traits, cos)
+    a = geno_min.get_GmY
+    dset_gy.append_1d(a)
+    b = geno_min.get_GmGm
+    dset_gy.append_1d(b)
+    c = geno_min.get_GmZc
+    dset_gz.append(c)
 
-        # beta, se, z_score, p = Reg.LinearRegression(mat, ped_pheno).cal_p()
-        # res = pd.DataFrame({'SNP': snp_id, 'Beta': beta, 'SE': se, 'Z-score': z_score, 'P': p}).round({'Beta': 6,
-        #                                                                                                'SE': 5,
-        #                                                                                                'Z-score': 6,
-        #                                                                                                'P': 6})
-        # df = df.append(res, ignore_index=True)
+    # beta, se, z_score, p = Reg.LinearRegression(mat, ped_pheno).cal_p()
+    # res = pd.DataFrame({'SNP': snp_id, 'Beta': beta, 'SE': se, 'Z-score': z_score, 'P': p}).round({'Beta': 6,
+    #                                                                                                'SE': 5,
+    #                                                                                                'Z-score': 6,
+    #                                                                                                'P': 6})
+    # df = df.append(res, ignore_index=True)
 
 
 
